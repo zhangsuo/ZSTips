8c7600f7e8932a9e598f26201ecfd83cb4dc2b23
zhangsuo <zhangsuo@zenking.cc>
2022-04-26 16:13:42 +0800

    V1.0.0

--- Example/ZSTips/ZSViewController.m
+++ Example/ZSTips/ZSViewController.m
@@ -7,7 +7,7 @@
 //
 
 #import "ZSViewController.h"
-#import "ZSTest.h"
+
 @interface ZSViewController ()
 
 @end
@@ -19,6 +19,7 @@
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
     
+    self.view.backgroundColor = [UIColor redColor];
 }
 
 - (void)didReceiveMemoryWarning
---
+++ ZSTips/Classes/EasyShow.h
@@ -0,0 +1,20 @@
+//
+//  EasyShow.h
+//  EasyShowViewDemo
+//
+//  Created by Mr_Chen on 2017/11/30.
+//  Copyright © 2017年 chenliangloveyou. All rights reserved.
+//
+// github上会实时更新：https://github.com/chenliangloveyou/EasyShowView
+// 喜欢的话给个start，你的start是我努力维护的动力
+#ifndef EasyShow_h
+#define EasyShow_h
+
+#import "EasyShowTextView.h"
+#import "EasyShowLodingView.h"
+#import "EasyShowAlertView.h"
+
+#import "EasyShowOptions.h"
+#import "EasyShowTypes.h"
+
+#endif /* EasyShow_h */
